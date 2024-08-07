255d10c77105ce825001a1c3e84ff56a57bd98a8
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-08-07 17:15:12 +0200

    ?+

--- src/component/2-hero/hero.jsx
+++ src/component/2-hero/hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
-            src="./2.png"
+            src="/2.png"
             alt="yousef~Aًٍََِْl~noumany"
             className="avatar"
           />
